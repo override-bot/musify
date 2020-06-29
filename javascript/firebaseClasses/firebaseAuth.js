f03e2c1914a5f276480993e447c3fc3c682bdc6d
Chukwudi Wisdom <override-bot@users.noreply.github.com>
2020-06-29 18:16:08 +0200

    update

--- javascript/firebaseClasses/firebaseAuth.js
+++ javascript/firebaseClasses/firebaseAuth.js
@@ -26,18 +26,59 @@ function handleSignUp() {
 
 }
 
+
+
+function toggleSignIn() {
+    if (firebase.auth().currentUser) {
+        // [START signout]
+        firebase.auth().signOut();
+        // [END signout]
+    } else {
+        var email = document.getElementById('email2').value;
+        var password = document.getElementById('password2').value;
+        if (email.length < 4) {
+            alert('Please enter an email address.');
+            return;
+        }
+        if (password.length < 4) {
+            alert('Please enter a password.');
+            return;
+        }
+        // Sign in with email and pass.
+        // [START authwithemail]
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // [START_EXCLUDE]
+            if (errorCode === 'auth/wrong-password') {
+                alert('Wrong password.');
+            } else {
+                alert(errorMessage);
+            }
+            console.log(error);
+
+            // [END_EXCLUDE]
+        });
+
+        // [END authwithemail]
+    }
+
+}
+
 function initApp() {
 
     firebase.auth().onAuthStateChanged(function(user) {
 
         if (user) {
 
-            window.location = 'index.html'
+            window.location.href = '../html/index.html';
 
         }
 
     });
-
+    document.getElementById('signin').addEventListener('click', toggleSignIn, false);
+    //document.getElementById('signup').addEventListener('click', handleSignUp, false);
 
 }
 
